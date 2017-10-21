47da058708683dffec289d8f338d7b6b3f2693ce
Andre Mattos <andremattos@gmail.com>
2017-10-21 17:28:39 -0200

    added cross drawing

--- soda/src/js/Particle.js
+++ soda/src/js/Particle.js
@@ -2,7 +2,7 @@ class Particle extends THREE.Mesh{
 	constructor (){
 		super();
 
-		this.add(this.circle());
+		this.add(this.cross());
 		
 		// this.rotation.y = Math.random() * Math.PI*2
 	}
@@ -27,6 +27,31 @@ class Particle extends THREE.Mesh{
 		shape.lineTo(5, 5);
 		shape.lineTo(0, 5);
 		shape.lineTo(-5, 5);
+
+		let geometry = new THREE.ShapeGeometry(shape);
+		let material = new THREE.MeshBasicMaterial({color:0xffffff});
+
+		return new THREE.Mesh(geometry, material);
+	}
+
+	cross(size = 10, thickness = 2){
+		let sHalf = size / 2;
+		let tHalf = thickness / 2;
+
+		let shape = new THREE.Shape();
+		shape.moveTo(-tHalf, -sHalf);
+		shape.lineTo(tHalf, -sHalf);
+		shape.lineTo(tHalf, -tHalf);
+		shape.lineTo(sHalf, -tHalf);
+		shape.lineTo(sHalf, tHalf);
+		shape.lineTo(tHalf, tHalf);
+		shape.lineTo(tHalf, sHalf);
+		shape.lineTo(-tHalf, sHalf);
+		shape.lineTo(-tHalf, tHalf);
+		shape.lineTo(-sHalf, tHalf);
+		shape.lineTo(-sHalf, -tHalf);
+		shape.lineTo(-tHalf, -tHalf);
+		shape.moveTo(-tHalf, -sHalf);
 
 		let geometry = new THREE.ShapeGeometry(shape);
 		let material = new THREE.MeshBasicMaterial({color:0xffffff});
